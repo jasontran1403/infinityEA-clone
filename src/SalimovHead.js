d08d38ab5abb4ece69933f63579eeb4cbec51de3
edzdw <minhtam09081999@gmail.com>
2023-08-06 14:46:28 +0700

    Bugs

--- src/SalimovHead.js
+++ src/SalimovHead.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 const SalimovHead = () => {
   return (
     <Head>
-      <title>InfinityEA-aaa</title>
+      <title>InfinityEA-Alex Group</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Template Google Fonts */}
