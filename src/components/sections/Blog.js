e385ab420707605b102d58a43a2d548cee8cd990
edzdw <minhtam09081999@gmail.com>
2023-08-06 14:09:26 +0700

    update

---
+++ src/components/sections/Blog.js
@@ -0,0 +1,115 @@
+import Link from "next/link";
+const Blog = () => {
+  return (
+    <section className="blog main-section flex-column-mobile" id="blog">
+      {/* TITLE STARTS */}
+      <div className="custom-title">
+        {/* MAIN TITLE STARTS */}
+        <h3>
+          <span>
+            <span className="animated-layer fade-in-left-animation fadeInUp wow">
+              Latest Posts
+            </span>
+          </span>
+        </h3>
+        {/* MAIN TITLE ENDS */}
+      </div>
+      {/* TITLE ENDS */}
+      {/* LATEST POSTS STARTS */}
+      <div className="latestposts flex-column-mobile">
+        {/* POST ITEM STARTS */}
+        <div className="animated-layer fade-in-right-animation fadeInUp wow">
+          <Link href="/blog-post" legacyBehavior>
+            <a>
+              <span className="img-holder">
+                <img src="assets/blog/blog-post-1.jpg" alt="" />
+              </span>
+              <div className="content">
+                <span className="category">Forex</span>
+                <span className="title">
+                  The meaning of currency pairs in Forex.
+                </span>
+                <p>
+                  Stay informed with the latest Forex news updates, enabling you to make more accurate investment decisions.
+                </p>
+                <div className="meta d-flex align-items-center">
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-calendar" />
+                    <span>9 Apr 2022</span>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-comments" />
+                    <span>17 comments</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+        {/* POST ITEM ENDS */}
+        {/* POST ITEM STARTS */}
+        <div className="animated-layer fade-in-right-animation fadeInUp wow">
+          <Link href="/blog-post2" legacyBehavior>
+            <a>
+              <span className="img-holder">
+                <img src="assets/blog/blog-post-2.jpg" alt="" />
+              </span>
+              <div className="content">
+                <span className="category">Exness</span>
+                <span className="title">
+                  Why Choose Exness: Unveiling the Reasons
+                </span>
+                <p>
+                  Exness is one of the leading and reputable forex brokers in the industry. Established in 2008
+                </p>
+                <div className="meta d-flex align-items-center">
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-calendar" />
+                    <span>18 Jan 2023</span>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-comments" />
+                    <span>34 comments</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+        {/* POST ITEM ENDS */}
+        {/* POST ITEM STARTS */}
+        <div className="animated-layer fade-in-right-animation fadeInUp wow">
+          <Link href="/blog-post3" legacyBehavior>
+            <a>
+              <span className="img-holder">
+                <img src="assets/blog/blog-post-3.jpg" alt="" />
+              </span>
+              <div className="content">
+                <span className="category">About us</span>
+                <span className="title">
+                  Infinity EA - aiming for infinity of Trading Market.
+                </span>
+                <p>
+                  Why Infinity EA - Exploring the Significance of Our Name. When our started, and goal ?
+                </p>
+                <div className="meta d-flex align-items-center">
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-calendar" />
+                    <span>1 Jan 2022</span>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-comments" />
+                    <span>10 comments</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+        {/* POST ITEM ENDS */}
+      </div>
+      {/* LATEST POSTS ENDS */}
+    </section>
+  );
+};
+export default Blog;
---
+++ src/components/sections/Facts.js
@@ -0,0 +1,89 @@
+const Facts = () => {
+  return (
+    <section className="facts">
+      <div className="flex-column-mobile">
+        {/* SINGE FACT ITEM STARTS */}
+        <div
+          className="animated-layer fade-in-right-animation fadeInLeft wow"
+          data-wow-offset={200}
+        >
+          <div>
+            <div>
+              <h3>56+</h3>
+              <p>
+                month of<span>experience</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* SINGE FACT ITEM ENDS */}
+        {/* SINGE FACT ITEM STARTS */}
+        <div
+          className="animated-layer fade-in-right-animation fadeInRight wow"
+          data-wow-offset={200}
+        >
+          <div>
+            <div>
+              <h3>10%</h3>
+              <p>
+                Profits<span>per Month</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* SINGE FACT ITEM ENDS */}
+        {/* SINGE FACT ITEM STARTS */}
+        <div
+          className="animated-layer fade-in-right-animation fadeInLeft wow"
+          data-wow-offset={200}
+        >
+          <div>
+            <div>
+              <h3>50%</h3>
+              <p>
+                Protection<span>Level 2</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* SINGE FACT ITEM ENDS */}
+        {/* SINGE FACT ITEM STARTS */}
+        <div
+          className="animated-layer fade-in-right-animation fadeInRight wow"
+          data-wow-offset={200}
+        >
+          <div>
+            <div>
+              <h3>30%</h3>
+              <p>
+                Protection<span>Lever 1</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* SINGE FACT ITEM ENDS */}
+        {/* SINGE FACT ITEM STARTS */}
+        <div
+          className="animated-layer fade-in-right-animation fadeInLeft wow"
+          data-wow-offset={200}
+        >
+          <div>
+            <div>
+              <h3>100+</h3>
+              <p>
+                Customer on <span>Mekong Delta Area</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* SINGE FACT ITEM ENDS */}
+      </div>
+      <img
+        alt=""
+        className="z-1 hide-mobile opposite-separator"
+        src="assets/separator-opposite.png"
+      />
+    </section>
+  );
+};
+export default Facts;
