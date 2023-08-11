e793c7b761f43d1b2289cab75e46a642fa2c0a08
edzdw <minhtam09081999@gmail.com>
2023-08-11 15:13:26 +0700

    Bugs

--- src/components/sections/Blog.js
+++ src/components/sections/Blog.js
@@ -48,38 +48,7 @@ const Blog = () => {
         </div>
         {/* POST ITEM ENDS */}
 
-        {/* POST ITEM STARTS */}
-        <div className="animated-layer fade-in-right-animation fadeInUp wow">
-          <Link href="/blog-post" legacyBehavior>
-            <a>
-              <span className="img-holder">
-                <img src="assets/blog/blog-post-1.jpg" alt="" />
-              </span>
-              <div className="content">
-                <span className="category">Forex</span>
-                <span className="title">
-                  The meaning of currency pairs in Forex.
-                </span>
-                <p>
-                  Stay informed with the latest Forex news updates, enabling you to make more accurate investment decisions.
-                </p>
-                <div className="meta d-flex align-items-center">
-                  <div className="d-flex align-items-center">
-                    <i className="fa-regular fa-calendar" />
-                    <span>9 Apr 2022</span>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <i className="fa-regular fa-comments" />
-                    <span>17 comments</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </Link>
-        </div>
-        {/* POST ITEM ENDS */}
-
-
+        
         {/* POST ITEM STARTS */}
         <div className="animated-layer fade-in-right-animation fadeInUp wow">
           <Link href="/blog-post2" legacyBehavior>
@@ -140,6 +109,39 @@ const Blog = () => {
           </Link>
         </div>
         {/* POST ITEM ENDS */}
+
+        {/* POST ITEM STARTS */}
+        <div className="animated-layer fade-in-right-animation fadeInUp wow">
+          <Link href="/blog-post4" legacyBehavior>
+            <a>
+              <span className="img-holder">
+                <img src="assets/blog/blog-post-1.jpg" alt="" />
+              </span>
+              <div className="content">
+                <span className="category">Forex</span>
+                <span className="title">
+                  The meaning of currency pairs in Forex.
+                </span>
+                <p>
+                  Stay informed with the latest Forex news updates, enabling you to make more accurate investment decisions.
+                </p>
+                <div className="meta d-flex align-items-center">
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-calendar" />
+                    <span>9 Apr 2022</span>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    <i className="fa-regular fa-comments" />
+                    <span>17 comments</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+        {/* POST ITEM ENDS */}
+
+
       </div>
       {/* LATEST POSTS ENDS */}
     </section>
